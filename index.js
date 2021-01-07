23669a4ee5bbc80c8d344571cde07d36d384d30a
Ana Hernandez <42675275+AnaHdezJ@users.noreply.github.com>
2021-01-06 22:20:43 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const os = require('os')
+
+const app = express()
+app.get('/', (req, res) =>{
+    res.send(`Hi from ${os.hostname()}!`)
+})
+
+const port =3000
+
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
+
+
